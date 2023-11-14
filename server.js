a200212746fff69a8f828f5598a45c850bf7f6bd
maks <maks.marolt@gmail.com>
2023-11-14 11:53:38 +0100

    filter deluje preko postman

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express, {json, request} from "express";
 import cors from "cors";
 var app = express();
-app.use(cors());
+app.use(cors(),bodyParser.json());
 var server = app.listen(8080, function () {
     const host = server.address().address
     const port = server.address().port
@@ -11,6 +11,7 @@ var server = app.listen(8080, function () {
 
 import {MongoClient, ServerApiVersion} from "mongodb";
 import {getUri} from './module.mjs';
+import * as bodyParser from "express";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(getUri(), {
     serverApi: {
@@ -25,10 +26,9 @@ app.post('/users', async (req, res)=> {
         await client.connect();
         const query =client.db("sah").collection("users");
         const users=await query.find(req.body).toArray();
-        //console.log(req.query);
+        console.log(req.body);
 
-        console.log("users",users);
-        //res.json({requestBody: req.body})
+        //console.log("users",users);
         res.json(users);
         res.end();
 
