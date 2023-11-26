3fc94e1b0cc1716eebb7c6a474df3c19f2effca5
maks <maks.marolt@gmail.com>
2023-11-26 11:39:44 +0100

    interakcija dveh loenih spletnih strani

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var server = app.listen(8080, function () {
 })
 
 
-import {MongoClient, ServerApiVersion} from "mongodb";
+import {MongoClient, ObjectId, ServerApiVersion} from "mongodb";
 import {getUri} from './module.mjs';
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(getUri(), {
@@ -53,7 +53,7 @@ app.post('/login', async (req, res)=> {
 
 app.post('/register', async (req, res)=> {
     try {
-        if(req.body._id===undefined)
+        if(req.body._id===undefined||req.body.pass===undefined)
             throw new Error("Body is empty");
         await client.connect();
         await client.db("sah").collection("users").insertOne(req.body,(err,res)=>{
@@ -70,5 +70,61 @@ app.post('/register', async (req, res)=> {
         await client.close();
     }
 })
+app.post('/game_check', async (req, res)=> {
+    if(req.body._id===undefined)
+        throw new Error("Body is empty");
+    try {
+        await client.connect();
+        const query =client.db("sah").collection("games");
+        const lastMove=await query.find({_id :new ObjectId(req.body._id)}).sort({_id:-1}).limit(1).project({last_move: 1}).toArray();
+        console.log(lastMove);
+        res.json(lastMove);
+        res.end();
 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+})
+app.post('/game_start', async (req, res)=> {
+    try {
+        if(req.body.p1===undefined || req.body.p2===undefined)
+            throw new Error("Body is empty");
+        await client.connect();
+        var game={p1:req.body.p1,p2:req.body.p2,moves:"",last_move:""};
+        await client.db("sah").collection("games").insertOne(game,(err,res)=>{
+            if(err)throw err;
+        });
+        console.log("igra!");
+        res.send(game._id);
+        res.end();
 
+    } catch (e) {
+        console.error(e);
+        res.send({message:e.message})
+    } finally {
+        await client.close();
+    }
+})
+app.post('/game_move', async (req, res)=> {
+    try {
+        if(req.body._id===undefined || req.body.move===undefined)
+            throw new Error("Body is empty");
+        await client.connect();
+        const query =client.db("sah").collection("games");
+        const gameInfo=await query.find({_id :new ObjectId(req.body._id)}).sort({_id:-1}).limit(1).project({last_move: 1,moves:1,_id:0}).toArray();
+        let game={moves:gameInfo[0].moves+","+gameInfo[0].last_move,last_move:req.body.move};
+        await client.db("sah").collection("games").updateOne({_id :new ObjectId(req.body._id)},{$set: game},(err,res)=>{
+            if(err)throw err;
+        });
+        res.send("moved!");
+        res.end();
+
+    } catch (e) {
+        console.error(e);
+        res.send({message:e.message})
+    } finally {
+        await client.close();
+    }
+})
